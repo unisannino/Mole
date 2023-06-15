0034d37fdf7a1a02ac9c562b11347d9e7c20078f
unisannino <unisannino@gmail.com>
2023-06-16 03:47:52 +0900

    :sparkles: Add Shuffle

--- Assets/Scripts/Runtime/Extensions/EnumerableExtension.cs
+++ Assets/Scripts/Runtime/Extensions/EnumerableExtension.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using UnityEngine;
 
 namespace Unisannino.Mole.Runtime.Extensions
 {
@@ -8,6 +9,17 @@ namespace Unisannino.Mole.Runtime.Extensions
         public static IEnumerable<(T, int)> WithIndex<T>(this IEnumerable<T> self)
         {
             return self.Select((item, index) => (item, index));
+        }
+        
+        public static void Shuffle<T>(this IList<T> self)
+        {
+            // fisher-yates shuffle
+            var n = self.Count;
+            for (var i = 0; i < n; i++)
+            {
+                var r = i + Random.Range(0, n - i);
+                (self[r], self[i]) = (self[i], self[r]);
+            }
         }
     }
 }
